0cc0eaace0df9c75aedbb96ae2300d2398473a85
Naetffy <mateosebastianforerofuentes@gmail.com>
2024-05-07 15:37:31 -0500

    before merge with juanes branch

--- src/main/java/co/edu/escuelaing/cvds/ClothCraft/controller/UserController.java
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/controller/UserController.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
