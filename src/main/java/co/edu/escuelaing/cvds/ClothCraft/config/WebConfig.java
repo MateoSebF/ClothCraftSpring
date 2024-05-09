edfa2333c3a962765b250a2da313df4abd91673d
Naetffy <mateosebastianforerofuentes@gmail.com>
2024-05-08 23:58:51 -0500

    5.1.1

--- src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/WebConfig.java
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/WebConfig.java
@@ -1,7 +1,6 @@
 package co.edu.escuelaing.cvds.ClothCraft.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -30,12 +29,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/");
     }
     
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3000/Wardrobe")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
