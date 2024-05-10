a5c622bfbc40cbc2e66af55c9cb89226efd60b95
Mateo_ForeroF <87267672+Naetffy@users.noreply.github.com>
2024-05-09 20:36:22 -0500

    Update CorsConfig.java

--- src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/CorsConfig.java
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package co.edu.escuelaing.cvds.ClothCraft.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
