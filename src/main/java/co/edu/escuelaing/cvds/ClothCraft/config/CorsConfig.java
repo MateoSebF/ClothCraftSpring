280cf3ee2c89888e346b9944387130cd7fb1cfb5
Naetffy <mateosebastianforerofuentes@gmail.com>
2024-05-10 23:48:58 -0500

    5.3.0

---
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package co.edu.escuelaing.cvds.ClothCraft.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(
+                Arrays.asList("http://localhost:3000", "https://mango-cliff-06b900910.5.azurestaticapps.net"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        @SuppressWarnings("unchecked")
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
--- src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/WebConfig.java
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/WebConfig.java
@@ -1,32 +1,21 @@
 package co.edu.escuelaing.cvds.ClothCraft.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SuppressWarnings("null") 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final BasicAuthInterceptor basicAuthInterceptor;
-
-    public WebConfig(BasicAuthInterceptor basicAuthInterceptor) {
-        this.basicAuthInterceptor = basicAuthInterceptor;
-    }
+    @Autowired
+    private BasicAuthInterceptor basicAuthInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(basicAuthInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/static/**");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
     }
 
 }
