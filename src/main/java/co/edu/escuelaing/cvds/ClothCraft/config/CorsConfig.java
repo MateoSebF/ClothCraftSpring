4c722035345a606f83b5ad0db5b7d678f33b1387
unknown <erick.montero@labinfo.is.escuelaing.edu.co>
2024-05-11 10:26:55 -0500

    funciona

---
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package co.edu.escuelaing.cvds.ClothCraft.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://mango-cliff-06b900910.5.azurestaticapps.net")
+                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization", "X-Requested-With", "X-CSRF-Token")
+                .allowCredentials(true)
+                .exposedHeaders("Access-Control-Allow-Origin"); // Exponer el encabezado Access-Control-Allow-Origin
+    }
+
+}
---
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/service/UserService.java
@@ -0,0 +1,70 @@
+package co.edu.escuelaing.cvds.ClothCraft.service;
+
+import co.edu.escuelaing.cvds.ClothCraft.model.User;
+import co.edu.escuelaing.cvds.ClothCraft.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+import co.edu.escuelaing.cvds.ClothCraft.model.Session;
+import co.edu.escuelaing.cvds.ClothCraft.repository.SessionRepository;
+
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SessionService sessionService;
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User user) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            user.setId(existingUser.getId());
+            return userRepository.save(user);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteUser(String id) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            userRepository.delete(existingUser);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User getUserByEmail(String email) {return userRepository.findByEmail(email).orElse(null);}
+
+    public User getUserByUserName(String username) {return userRepository.findByUsername(username).orElse(null);}
+
+    public User getUserByToken(UUID token) {
+        Session session = sessionService.getSessionByToken(token);
+        if (session != null) {
+            return session.getUser();
+        } else {
+            return null;
+        }
+    }
+}
+
