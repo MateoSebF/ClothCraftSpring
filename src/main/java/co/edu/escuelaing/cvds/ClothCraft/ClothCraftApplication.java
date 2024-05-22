6cacda2fd27e663fbd93dc6e18a17fce9feac8c3
unknown <mateo.forero-f@labinfo.is.escuelaing.edu.co>
2024-05-22 12:19:38 -0500

    6.6.0

--- src/main/java/co/edu/escuelaing/cvds/ClothCraft/ClothCraftApplication.java
+++ src/main/java/co/edu/escuelaing/cvds/ClothCraft/ClothCraftApplication.java
@@ -41,7 +41,7 @@ public class ClothCraftApplication {
 		};
 	}
 
-	@Scheduled(cron = "0 15 13 * * ?") // Programa la tarea para que se ejecute a las 12 PM todos los días
+	@Scheduled(cron = "0 20 12 * * ?") // Programa la tarea para que se ejecute a las 12 PM todos los días
     public void sendDailyNotifications() {
         try {
             // Obtén todas las notificaciones pendientes
